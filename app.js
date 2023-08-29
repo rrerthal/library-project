4c0f0f7fa24c05bfc87322de2204a1e5b2dae030
Rodrigo Erthal <rrerthal@gmail.com>
2023-08-29 15:36:11 +0200

    add author route

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ app.use("/", indexRoutes);
 const bookRoutes = require("./routes/book.routes");
 app.use("/", bookRoutes);
 
+const authorRoutes = require("./routes/author.routes")
+app.use("/", authorRoutes)
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
---
+++ routes/author.routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const Author = require("../models/Author.model");
+
+
+router.get("/authors", (req, res, next) => {
+    Author.find()
+        .then( authorsFromDB => {
+            const data = {
+                authors: authorsFromDB
+            }
+            res.render("authors/authors-list", data);
+        })
+        .catch((e) => {
+            console.log("Error getting list of authors from DB", e);
+            next(e);
+        });
+});
+  module.exports = router;
